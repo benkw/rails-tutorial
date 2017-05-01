a529609af2b633cab8d8ded5f3ad04bda10573f1
ben <ben.kb.wang@gmail.com>
2017-05-01 07:08:50 +0000

    Finish user singup

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get '/help',    to: 'static_pages#help', as: 'helf'
   get '/about',   to: 'static_pages#about'
   get '/signup',  to: 'users#new'
+  post '/signup', to: 'users#create'
   root 'static_pages#home'
+  resources :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
